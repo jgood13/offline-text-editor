d8ec171bb56077ae9a7e1164759cba3f5e4d9d3c
jgood13 <joseph.good8@gmail.com>
2022-09-21 15:04:43 -0600

    deploying to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,61 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  // We are creating a new database named 'jate' which will be using version 1 of the database.
+  openDB("jate", 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const getDb = async () => {
+  console.log("GET from the database");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB("jate", 1);
 
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("data saved", result);
+};
+
+// Export a function we will use to update to the database.
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the put method on the store and pass in the content.
+  const request = store.put({
+    content: content,
+  });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+
+// Start the database.
 initdb();
